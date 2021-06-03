5a37b1dd44b5e7ecd3c4e4e791194ad73e9016ec
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-03 16:38:42 -0700

    Add correct operators

--- Js/script.js
+++ Js/script.js
@@ -13,13 +13,13 @@ $(document).ready(function() {
     if(total <=4) {
       $("#first1").show()
    
-  } else if (total <=5 && total >=9) {
+  } else if (total >=5 && total <=9) {
       $("#second2").show()
     
-    } else if (total =>10 && total >=15) {
+    } else if (total >=10 && total <=13) {
       $("#third3").show()
   
-    } else if (total >=16 && total<=21) {
+    } else if (total >=12 && total<=16) {
     $("#forth4").show()
 }
     
