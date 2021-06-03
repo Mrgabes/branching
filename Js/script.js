71423316201ba74eabe1b970c4d41b82f6d0012d
Gabriel Rodriguez <mrgabes72@gmail.com>
2021-06-03 16:06:42 -0700

    Add gh-pages

--- Js/script.js
+++ Js/script.js
@@ -1,26 +1,25 @@
 
 $(document).ready(function() {
-  $("#dating").submit(function(event) {
+  $("#datingForm").submit(function(event) {
     event.preventDefault();
   
-    let firstQuestion = parseInt($("#first").val())
-    let secondQuestion = parseInt($("#second").val())
-    let thirdQuestion = parseInt($("#third").val())
-    let forthQuestion = parseInt($("#forth").val())
+    let firstQuestion = parseInt($("#firstDate").val())
+    let secondQuestion = parseInt($("#secondDate").val())
+    let thirdQuestion = parseInt($("#thirdDate").val())
+    let forthQuestion = parseInt($("#forthDate").val())
     let total = firstQuestion + secondQuestion + thirdQuestion + forthQuestion;
   
-    console.log(total);
-    if(total <=4) {
-      $("#first1").show()
-   
-  } else if (total <=5 && total >=9) {
-      $("#second2").show()
-    
-    } else if (total =>10 && total >=15) {
-      $("#third3").show()
+    console.log("total",total)
+    if(total === 4) {
+      $("#firstOne").show()
+      console.log("helllleeee00")
+    } else if (total >=5 && total <=8) {
+      $("#secondOne").show()
+    } else if (total >=9 && total <=12) {
+      $("#thirdOne").show()
   
-    } else if (total >=16 && total<=21) {
-    $("#forth4").show()
+    } else if (total >=13 && total<=16) {
+    $("#forthOne").show()
 }
     
 });
